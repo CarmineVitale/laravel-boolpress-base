1df7b2ecaeb1366a3c31b4cbcc3b4d1b49deca98
CarmineV <carmine.vitale90@gmail.com>
2020-06-23 17:30:52 +0200

    Fix - richiesta esercizio completata

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,7 +6,19 @@
             <h3>Titolo: {{ $post->title }}</h3>
             <h4>Scritto da: {{ $post->user->name }}</h4>
             <p>{{ $post->description }}</p>
+            <h4>Commenti</h4>
+            <ul>
+                @forelse ($post->comments as $item)
+                <li> <small>Titolo commento:</small>{{$item->title}} <br> <small>Corpo commento:</small> {{$item->body}} </li>
+                @empty
+                   <h2>Commenti non presenti</h2>
+                @endforelse
+            </ul>
+            
+            {{-- //@dump($post->comments) --}}
+            
             @if (!$loop->last) <hr> @endif
+            
         </div>
     @endforeach
     {{-- Impaginazione posts --}}
